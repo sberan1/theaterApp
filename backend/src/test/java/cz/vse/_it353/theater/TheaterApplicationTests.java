5b800ffe10788925561aa1c592be81777189d9a3
sberan1 <stepan.beran@outlook.com>
2024-05-29 06:53:38 +0200

    init

---
+++ backend/src/test/java/cz/vse/_it353/theater/TheaterApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.vse._it353.theater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
